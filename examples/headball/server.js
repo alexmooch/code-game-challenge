ba57f8a473e8bedb29fd767b33e25af3db857be3
alexmooch <alex.mooch@list.ru>
2016-09-17 20:07:23 +0300

    Removed unnecessary example files

--- examples/headball/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-var app = express();
-app.configure(function () {
-    // app.use(express.logger('dev'));
-    app.use(express.static(path.join(__dirname, 'public')));
-});
